e5618fa70716ba11df8dc1bc3c68513cdff2a049
Dmitrii <d.martynov@dev.userstory.ru>
2021-03-06 21:18:48 +0300

    started

---
+++ src/models/User.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { Eventing } from "./Eventing";
+
+interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  public events: Eventing = new Eventing();
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    this.data = { ...update };
+  }
+}
